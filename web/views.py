dd7b7dfd585857080dc73537827f39d440f9d5a5
Gegegedo <wlm1994@live.com>
2018-10-24 16:05:30 +0800

    First Commit

--- web/views.py
+++ web/views.py
@@ -47,12 +47,6 @@ def _resource_search(request):
                   template_name='rm_resource_search.html')
 
 def index_new(request):
-    with open(os.path.abspath('../Demo/Maps/8/geojsons_t1.json')) as json_file:
-        # print(str(json.load(json_file)[0]['geometry']))
-        P=fromstr(str(json.load(json_file)[0]['geometry']))
-        #P=GEOSGeometry('{ "type": "Polygon", "coordinates": [[ 5.000000, 23.000000 ],[5.0,20.0],[8.0,22.0]] }')
-        m = Mask(code='test', poly=P)
-        m.save()
     return render(request,
                   template_name='index_new.html')
 
