52ac6dfa97978191c3549d539b50cb3217adaeb4
Gegegedo <wlm1994@live.com>
2018-10-29 19:16:02 +0800

    Merge branch 'master' of https://github.com/DarryNobi/Demo
    # Conflicts:
    #	static/js/ib_event_management_showlist.js
    #	templates/ib_plotting.html
    #	web/views.py

--- static/js/roller_shutters.js
+++ static/js/roller_shutters.js
@@ -1,4 +1,6 @@
 $(function(){
+    var demolition;
+    var ibuild;
 
     selection1=document.getElementById("compare1");
     selection2=document.getElementById("compare2");
@@ -9,38 +11,97 @@ $(function(){
             data: {
             },
             success:function(d_maps){
-                maps_list=JSON.parse(d_maps);
-                for(m in maps_list){
-                    area=maps_list[m].Area;
-                    time=maps_list[m].ReceiveTime;
-                    id=maps_list[m].GlobeID;
+                maps_list=JSON.parse(d_maps['maps']);
+                maps_list=maps_list['maps']
+                console.log(maps_list[0]['id']);
+                console.log(typeof(d_maps));
+               // maps_list=map_list["maps"];
+                for(var m in maps_list){
+                    area=maps_list[m]['area'];
+
+                    time=maps_list[m]['capture_time'];
+                    id=maps_list[m]['id'];
+
                     selection1.add(new Option(area+time,id))
 
                     ////////////////////////////////////////////add all layers by default
-                    temp_layer = new ol.layer.Image({
-                        source: new ol.source.ImageWMS({
-                          crossOrigin: 'anonymous',
-                            url:'http://172.20.53.158:8080/geoserver/wms',
-                            projection:'EPSG:4326',
-                            params:{
-                            LAYERS: id.toString()}
-                        }),
-                        projection: "EPSG:4326",
-                       // opacity:0.5,
-                    });
-                    //map.addLayer(temp_layer);
+//                    temp_layer = new ol.layer.Image({
+//                        source: new ol.source.ImageWMS({
+//                          crossOrigin: 'anonymous',
+//                            url:'http://172.20.53.157:8080/geoserver/wms',
+//                            projection:'EPSG:4326',
+//                            params:{
+//                            LAYERS: 'Map:'+id.toString()}
+//                        }),
+//                        projection: "EPSG:4326",
+//                       // opacity:0.5,
+//                    });
+//                    map.addLayer(temp_layer);
                 }
             }
          });
+          $.ajax({
+        type:'post',
+        url:'pattern_change_detection',
+        data:{'img1':value1,'img2':value2},
+        success:function(result){
+          ibuild_area=JSON.parse(result['ibuild_area']);
+          demolition_area=JSON.parse(result['demolition_area']);
+          ibuild_area['geometry']=JSON.parse(ibuild_area['geometry']);
+          demolition_area['geometry']=JSON.parse(demolition_area['geometry']);
+
+           var ibuild_features=(new ol.format.GeoJSON()).readFeatures(ibuild_area);
+            style1=new ol.style.Style({
+                stroke:new ol.style.Stroke({
+                    color:'#00FFFF',
+                   width: 1,
+                }),
+                fill: new ol.style.Fill({ //矢量图层填充颜色，以及透明度
+                    color: '#A6C2DE'
+                }),
+            });
+            ibuild_features[0].setStyle(style1);
+            var ibuild_vectorSource = new ol.source.Vector({
+                features: ibuild_features//(new ol.format.GeoJSON()).readFeatures(ibuild_draws[i])
+              });
+
+            var ibuild_vectorLayer = new ol.layer.Vector({
+                source: ibuild_vectorSource,
+                 opacity:0.5
+            });
+
+
+            var demolition_features=(new ol.format.GeoJSON()).readFeatures(demolition_area);
+            style2=new ol.style.Style({
+                stroke:new ol.style.Stroke({
+                    color:  '#FFFFFF',
+                   width: 1,
+                }),
+                fill: new ol.style.Fill({ //矢量图层填充颜色，以及透明度
+                    color: '#FFED59'
+                }),
+            });
+            demolition_features[0].setStyle(style2);
+            var demolition_vectorSource = new ol.source.Vector({
+                features: demolition_features//(new ol.format.GeoJSON()).readFeatures(ibuild_draws[i])
+              });
+
+            var demolition_vectorLayer = new ol.layer.Vector({
+                source: demolition_vectorSource,
+                 opacity:0.5
+            });
+          }
+        });
+
     selection1.onchange=function(){
         var index1 = selection1.selectedIndex;
         $("#compare2").empty();
         for(m in maps_list){
             area1=selection1.options[index1].text.replace(/[0-9]/g, '').replace(/[-]/g, '');
-            if(maps_list[m].Area==area1){
-                var area=maps_list[m].Area;
-                var time=maps_list[m].ReceiveTime;
-                    id=maps_list[m].GlobeID;
+            if(maps_list[m]['area']==area1){
+                var area=maps_list[m]['area'];
+                var time=maps_list[m]['capture_time'];
+                var  id=maps_list[m]['id'];
                 selection2.add(new Option(area+time,id));
             }
         }
@@ -56,7 +117,8 @@ $(function(){
     if(iscompare){
         map.removeLayer(temp_layer1);
         map_2.removeLayer(temp_layer2);
-
+        map_2.removeLayer(ibuild_vectorLayer);
+        map_2.removeLayer(demolition_vectorLayer);
         map2.hide();
         map1.off('mousemove');
         map2.off('mousemove');
@@ -70,8 +132,8 @@ $(function(){
         var index2 = selection2.selectedIndex;
         var text2 = selection2.options[index2].text;
         var value2 = selection2.options[index2].value;
-        //alert(value1);
-        //alert(value2);
+
+
         for(m in maps_list){
                     id=maps_list[m].GlobeID;
                     if(id==value2){
@@ -85,10 +147,10 @@ $(function(){
         temp_layer1 = new ol.layer.Image({
             source: new ol.source.ImageWMS({
               crossOrigin: 'anonymous',
-                url:'http://172.20.53.158:8080/geoserver/wms',
+                url:'http://172.20.53.157:8080/geoserver/wms',
                 projection:'EPSG:4326',
                 params:{
-                LAYERS: value1.toString()}
+                LAYERS: 'Map:'+value1.toString()}
             }),
             projection: "EPSG:4326",
            // opacity:0.5,
@@ -96,15 +158,16 @@ $(function(){
         temp_layer2 = new ol.layer.Image({
             source: new ol.source.ImageWMS({
               crossOrigin: 'anonymous',
-                url:'http://172.20.53.158:8080/geoserver/wms',
+                url:'http://172.20.53.157:8080/geoserver/wms',
                 projection:'EPSG:4326',
                 params:{
-                LAYERS: value2.toString()}
+                LAYERS: 'Map:'+value2.toString()}
             }),
             projection: "EPSG:4326",
             //opacity:0.5,
         });
-
+        map_2.addLayer(ibuild_vectorLayer);
+        map_2.addLayer(demolition_vectorLayer);
         map.addLayer(temp_layer2);
         map_2.addLayer(temp_layer1);
 
@@ -123,11 +186,11 @@ $(function(){
         //console.log(e);
         e.stopPropagation();
         var  offsetX=e.pageX,offsetY=e.y,width=document.body.clientWidth,height=document.body.clientHeight;
-        console.log(offsetX);
+        //console.log(offsetX);
         document.getElementById('map_2').style.clip='rect(0px,'+offsetX+'px,'+height+'px,0px)';
     }
     function event_map2(e){
-        console.log(e);
+      //  console.log(e);
         e.stopPropagation();
         var  offsetX=e.pageX,offsetY=e.y,width=document.body.clientWidth,height=document.body.clientHeight;
         document.getElementById('map_2').style.clip='rect(0px,'+offsetX+'px,'+height+'px,0px)';
